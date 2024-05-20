2c8b67bf62612cb5b0ed6cd30df8bd3058595d59
SiriusKoan <me@siriuskoan.one>
2024-05-20 06:39:27 +0000

    feat(web): init

---
+++ web/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-blue-100">Welcome to my App!</h1>
+    <p>This is the main component of my application.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
