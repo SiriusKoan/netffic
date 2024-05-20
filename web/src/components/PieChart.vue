6bff8a3e525672173104072627084e66b4a796eb
SiriusKoan <me@siriuskoan.one>
2024-05-20 13:52:31 +0000

    feat(web): L7 page

---
+++ web/src/components/PieChart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="inline-flex bg-opacity-40 bg-white items-center justify-center p-3">
+        <apexchart type="pie" :options="chartOptions" :series="chartSeries" width="500" />
+    </div>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+    name: 'PieChart',
+    components: {
+        apexchart: VueApexCharts,
+    },
+    props: {
+        chartOptions: Object,
+        chartSeries: Array,
+    },
+};
+</script>
--- web/src/views/L7View.vue
+++ web/src/views/L7View.vue
@@ -1,8 +1,42 @@
 <template>
+    <h2 class="text-center">HTTP</h2>
+    <div class="flex flex-wrap gap-2">
+        <PieChart v-for="(chartOptions, index) in chartOptionses" :key="index" :chartOptions="chartOptions" :chartSeries="chartSerieses[index]" />
+    </div>
 </template>
 
 <script>
+import PieChart from '../components/PieChart.vue';
 export default {
     name: 'L7View',
+    components: {
+        PieChart,
+    },
+    data() {
+        return {
+            chartOptionses: [],
+            chartSerieses: [],
+        };
+    },
+    mounted() {
+        setInterval(() => {
+            fetch('http://work.isk:8000/api/stats/http')
+                .then(response => response.json())
+                .then(data => {
+                    let fields = ["contentTypeCounter", "hostCounter", "methodCounter", "statusCounter", "userAgentCounter"];
+                    this.chartOptionses = fields.map(field => {
+                        return {
+                            chart: {
+                                id: field,
+                            },
+                            labels: Object.keys(data[field]),
+                        };
+                    });
+                    this.chartSerieses = fields.map(field => {
+                        return Object.values(data[field]);
+                    });
+                });
+        }, 1000);
+    },
 }
 </script>
